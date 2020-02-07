097da660754c99ae1ff1c2649e8c770b4626ca5c
Florian Wartner <florian@wartner.io>
2020-02-07 21:51:19 +0100

    Added Images

--- resources/views/_includes/meta.blade.php
+++ resources/views/_includes/meta.blade.php
@@ -9,3 +9,15 @@
 <meta name="msapplication-TileColor" content="#da532c">
 <meta name="msapplication-TileImage" content="/mstile-144x144.png">
 <meta name="theme-color" content="#ffffff">
+<meta name="description" content="Use snippets made by the community right in Tinkerwell. Or create & share your own.">
+<meta name="keywords" content="tinkerwell, php, app, snippets">
+<meta name="robots" content="index, follow">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta name="language" content="English">
+<meta property="og:title" content="Use snippets made by the community right in Tinkerwell. Or create & share your own.">
+<meta property="og:site_name" content="Tinkerwell Snippets">
+<meta property="og:url" content="https://tinkerwell-snippets.com">
+<meta property="og:description" content="">
+<meta property="og:type" content="product">
+<meta property="og:image" content="https://tinkerwell-snippets.com/images/og_image.png">
+
