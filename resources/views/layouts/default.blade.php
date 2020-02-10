d13db36496fcca07bc3291807b604a859af97fce
Florian Wartner <florian@wartner.io>
2020-02-10 09:43:26 +0100

    Added tag-manager

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -12,11 +12,22 @@
     <title>@yield('page_title') - {{ config('app.name') }}</title>
 
     <script src="https://kit.fontawesome.com/1aad535f8f.js" crossorigin="anonymous"></script>
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-NM232NW');</script>
+    <!-- End Google Tag Manager -->
 
     <!-- Styles -->
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 </head>
 <body class="font-mono bg-gray-800">
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NM232NW"
+                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
     <div id="app">
         @include('_includes.navigation')
 
